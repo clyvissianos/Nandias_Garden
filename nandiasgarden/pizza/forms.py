2dcd9ebc24dfbc6a20388bdccbb310966fbcd29a
clyvissianos <c.lyvissianos@gmail.com>
2023-05-29 11:00:03 +0300

    Created with Django Forms

---
+++ nandiasgarden/pizza/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Pizza, Size
+
+class PizzaForm(forms.ModelForm):
+
+    class Meta:
+        model = Pizza
+        fields = ['topping1','topping2','size']
+        labels = {'topping1':'Topping 1','topping2':'Topping 2'}
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
+        
